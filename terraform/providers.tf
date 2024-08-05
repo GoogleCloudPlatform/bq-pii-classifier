12c0a5c4d9bdfa27a685b8b854c764b45a30d5c5
Karim Wadie <wadie@google.com>
2024-08-05 15:55:47 +0200

    added provider to all tf submodules due to tflint

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,5 @@
+# adding provider block in all modules for tflint
+# https://github.com/terraform-linters/tflint-ruleset-terraform/blob/v0.2.2/docs/rules/terraform_required_providers.md
 terraform {
   required_providers {
     google = "= 5.20.0"
